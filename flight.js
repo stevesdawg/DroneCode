79442bdf3ab234cf244d82e4221a3b45146e3153
stevesdawg <shrivathsav.seshan@gmail.com>
2016-11-06 10:14:31 -0500

    controller done???

--- flight.js
+++ flight.js
@@ -9,6 +9,7 @@ var drone = arDrone.createClient();
 // setTimeout(function(){drone.land()}, 5000);
 
 var pngStream = drone.getPngStream();
+var count = 0;
 
 var lastPng;
 pngStream
@@ -25,9 +26,13 @@ var server = http.createServer(function(req, res) {
             jsonObj = JSON.parse(data);
             if (jsonObj.boxheight == -1) {
                 drone.stop();
-                setTimeout(function() {drone.land()}, 3000);
+                count += 1;
+                if (count > 20) {
+                    drone.land();
+                }
             }
             else {
+                count = 0;
                 var centerx = 200;
                 var centery = 225/2.0;
                 if (jsonObj.boxcenter[0] < centerx) {
@@ -39,7 +44,14 @@ var server = http.createServer(function(req, res) {
                 if (jsonObj.boxcenter[1] < centery) {
                     drone.down((centery - jsonObj.boxcenter[1]) * 0.3 / centery);
                 } else {
-                    drone.up(jsonObj.boxcenter[1] - centery) * 0.3 / centery);
+                    drone.up((jsonObj.boxcenter[1] - centery) * 0.3 / centery);
+                }
+
+                var scaleFactor = jsonObj.boxheight / 210.0;
+                if (scaledFactor > 1) {
+                    drone.back(0.4 * scaledFactor);
+                } else {
+                    drone.front(0.4 * 1/scaledFactor)
                 }
             }
         });
@@ -55,6 +67,17 @@ var server = http.createServer(function(req, res) {
 
     res.writeHead(200, {'Content-Type': 'image/png'});
     res.end(lastPng);
+});
+
+var stdin = process.openStdin();
+require('tty').setRawMode(true);
+var rl = require('readline');
+
+stdin.on('keypress', function (chunk, key) {
+    process.stdout.write('Get Chunk: ' + chunk + '\n');
+    if (key && key.name == ' ') {
+        drone.land();
+    }
 });
 
 server.listen(8080, function() {
